ffc94c1e31f758be7452df7f709e085ec0b1ffc0
Camila Nino Francia <67711670+CCEW@users.noreply.github.com>
2026-05-22 10:31:41 -0700

    add C's files

---
+++ camila's function files/pir_sensor.h
@@ -0,0 +1,10 @@
+#ifndef PIR_SENSOR_H
+#define PIR_SENSOR_H
+
+#include <stdint.h>
+
+void PIR_Init(void);
+int PIR_MotionDetected(void);
+
+#endif // PIR_SENSOR_H
+
---
+++ camila's function files/ui.h
@@ -0,0 +1,23 @@
+#ifndef UI_H
+#define UI_H
+
+#include <stdint.h>
+
+typedef enum {
+    UI_STATE_IDLE = 0,
+    UI_STATE_GET_READY,
+    UI_STATE_RECORDING,
+    UI_STATE_PROCESSING,
+    UI_STATE_UPLOADING,
+    UI_STATE_PASS,
+    UI_STATE_FAIL,
+    UI_STATE_ENROLLED,
+    UI_STATE_CLEARED
+} UIState_t;
+
+void UI_Init(void);
+void UI_ShowState(UIState_t state, int user_id, int score);
+
+#endif // UI_H
+
+
